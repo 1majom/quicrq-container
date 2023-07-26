1516c2672c9371f5dc77487c2fae4314cac284e3
szebala <szebala@sch.bme.hu>
2023-07-26 13:27:53 +0200

    yamls kinda

--- containers/make-containers.sh
+++ containers/make-containers.sh
@@ -1,8 +1,12 @@
 docker  build -t 1majom/quicrq:server -f Dockerfile-server .
 docker  build -t 1majom/quicrq:relay -f Dockerfile-relay .
 docker  build -t 1majom/quicrq:relay-kube -f Dockerfile-relay-kube .
+docker  build -t 1majom/quicrq:relay-kube-in -f Dockerfile-relay-kube-in .
+docker  build -t 1majom/quicrq:relay-kube-out -f Dockerfile-relay-kube-out .
 docker  build -t 1majom/quicrq:client-receive -f Dockerfile-client-receive .
 docker  push 1majom/quicrq:server 
 docker  push 1majom/quicrq:relay 
 docker  push 1majom/quicrq:relay-kube 
+docker  push 1majom/quicrq:relay-kube-in
+docker  push 1majom/quicrq:relay-kube-out
 docker  push 1majom/quicrq:client-receive 
